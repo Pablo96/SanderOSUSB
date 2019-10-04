bdb8791d11bcd382da1f5836c22cf081a20835e5
AdeRegt <AdeRegt@github.com>
2019-10-04 19:47:39 +0200

    backup

--- kernel/dev/EHCI.c
+++ kernel/dev/EHCI.c
@@ -2,25 +2,78 @@
 
 extern unsigned int uhci_locs[10];
 extern unsigned int uhci_locs_cnt;
+unsigned long baseaddr;
+unsigned long virtregaddr;
 unsigned long USBSTS;
+unsigned long USBCMD;
+int portscount;
 
 extern void ehciirq();
 void irq_ehci(){
+	outportb(0xA0,0x20);
+	outportb(0x20,0x20);
 	unsigned long dinges = ((unsigned long*)USBSTS)[0];
 	printf("EHCI: fire with status %x \n",dinges);
-	outportb(0x20,0x20);
-	outportb(0xA0,0x20);
+	for(int i = 0 ; i < portscount ; i++){
+		unsigned long valz = virtregaddr+0x44+(4*i-1);
+		unsigned long dtas = ((unsigned long*)valz)[0];
+		if(dtas&0x000FFF){
+			printf("EHCI: portcount #%x with value %x has a connection!!!\n",i,dtas);
+			// are we already resetting?
+			if(dtas&0b100000000){
+				printf("EHCI: portcount #%x EHCI hardware made port reset, stop it\n",i);
+				// stop the reset
+				dtas = ((unsigned long*)valz)[0];
+				unsigned long flagtype = 0b100000000;
+				dtas &= (~flagtype);
+				((unsigned long*)valz)[0] = dtas;
+			}
+			
+			printf("EHCI: portcount #%x force reset\n",i);
+			// now we tell how we want things...
+			dtas = ((unsigned long*)valz)[0];
+			dtas |= 0b111; 		 	// port on active
+			dtas |= 0b100000000;		// port reset
+			dtas |= 0b1000000000000; 	// port power on
+			dtas |= 0b10000000000000; 	// port ownership on
+			((unsigned long*)valz)[0] = dtas;
+			
+			// wait a bit...
+			printf("EHCI: portcount #%x wait\n",i);
+			getch();
+			printf("EHCI: portcount #%x endof wait\n",i);
+			
+			// stop the reset
+			dtas = ((unsigned long*)valz)[0];
+			unsigned long flagtype = 0b100000000;
+			dtas &= (~flagtype);
+			((unsigned long*)valz)[0] = dtas;
+			
+			printf("EHCI: portcount #%x end of reset\n",i);
+			
+			dtas = ((unsigned long*)valz)[0];
+			if(dtas&1){
+				printf("EHCI-PORT#%x: Initialisation complete with status %x \n",i,dtas);
+				//((unsigned long*)valz)[0] |= 0b01000000000000000; // output green when possible
+			}else{
+				printf("EHCI-PORT#%x: Initialisation failed with status %x \n",i,dtas);
+				if(dtas&0b010000000000){
+					printf("EHCI-PORT#%x: Device is a low speed device \n",i);
+				}
+			}
+		}
+	}
 }
 
 unsigned long hqbuffer[1024];
 
 void ehci_init(int bus,int slot,int function){
-	unsigned long baseaddr 		= getBARaddress(bus,slot,function,0x10);
+			baseaddr 	= getBARaddress(bus,slot,function,0x10);
 	unsigned long HCIVERSION 	= baseaddr+2;
 	unsigned long HCSPARAMS 	= baseaddr+4;
-	unsigned long virtregaddr 	= baseaddr;
-	virtregaddr 			+= ((unsigned char*)baseaddr)[0x00];
-	unsigned long USBCMD 		= virtregaddr+0x00;
+			virtregaddr 	= baseaddr;
+			virtregaddr	+= ((unsigned char*)baseaddr)[0x00];
+			USBCMD 		= virtregaddr+0x00;
 			USBSTS 		= virtregaddr+0x04;
 	unsigned long USBINTR 		= virtregaddr+0x08;
 	unsigned long FRINDEX 		= virtregaddr+0x0C;
@@ -58,7 +111,7 @@ void ehci_init(int bus,int slot,int function){
 	printf("EHCI: Capability Registers Length %x \n",((unsigned char*)baseaddr)[0x00]);
 	printf("EHCI: Host Controller Interface Version Number %x \n",((unsigned short*)HCIVERSION)[0x00]);
 	printf("EHCI: Structural Parameters %x \n",((unsigned long*)HCSPARAMS)[0x00]);
-	int portscount = ((unsigned long*)HCSPARAMS)[0x00]&0b01111;
+	portscount = ((unsigned long*)HCSPARAMS)[0x00]&0b01111;
 	printf("EHCI: Number of avail ports: %x \n",portscount);
 	printf("EHCI: USBCMD %x \n",((unsigned long*)USBCMD)[0]);
 	printf("EHCI: USBSTS %x \n",((unsigned long*)USBSTS)[0]);
@@ -74,70 +127,7 @@ void ehci_init(int bus,int slot,int function){
 			break;
 		}
 	}
-	for(int i = 0 ; i < portscount ; i++){
-		unsigned long valz = virtregaddr+0x44+(4*i-1);
-		unsigned long dtas = ((unsigned long*)valz)[0];
-		if(dtas&0x000FFF){
-			printf("EHCI: portcount #%x with value %x has a connection!!!\n",i,dtas);
-			// are we already resetting?
-			if(dtas&0b100000000){
-				printf("EHCI: portcount #%x EHCI hardware made port reset, stop it\n",i);
-				// stop the reset
-				dtas = ((unsigned long*)valz)[0];
-				unsigned long flagtype = 0b100000000;
-				dtas &= (~flagtype);
-				((unsigned long*)valz)[0] = dtas;
-			}
-			// wait a bit...
-			resetTicks();
-			while(1){
-				if(getTicks()==10){
-					break;
-				}
-			}
-			
-			// now we tell how we want things...
-			dtas = ((unsigned long*)valz)[0];
-			dtas |= 0b100; 		 	// port on active
-			dtas |= 0b100000000;		// port reset
-			dtas |= 0b1000000000000; 	// port power on
-			dtas |= 0b10000000000000; 	// port ownership on
-			((unsigned long*)valz)[0] = dtas;
-			
-			// wait a bit...
-			resetTicks();
-			while(1){
-				if(getTicks()==10){
-					break;
-				}
-			}
-			
-			// stop the reset
-			dtas = ((unsigned long*)valz)[0];
-			unsigned long flagtype = 0b100000000;
-			dtas &= (~flagtype);
-			((unsigned long*)valz)[0] = dtas;
-			
-			// wait a bit...
-			resetTicks();
-			while(1){
-				if(getTicks()==10){
-					break;
-				}
-			}
-			
-			dtas = ((unsigned long*)valz)[0];
-			if(dtas&1){
-				printf("EHCI-PORT#%x: Initialisation complete with status %x \n",i,dtas);
-				//((unsigned long*)valz)[0] |= 0b01000000000000000; // output green when possible
-			}else{
-				printf("EHCI-PORT#%x: Initialisation failed with status %x \n",i,dtas);
-				if(dtas&0b010000000000){
-					printf("EHCI-PORT#%x: Device is a low speed device \n",i);
-				}
-			}
-		}
-	}
+	
 	
 	printf("END OF OPERATIONS");
 	getch();
